862b807de28d88b41015fc9e0238e568cd11a784
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-11-27 19:09:03 +0530

    Create Day 5.py

---
+++ Day 5.py
@@ -0,0 +1,12 @@
+# The following lines assign the variable to the left of the = 
+# assignment operator with the values and arithmetic expressions 
+# on the right side of the = assignment operator.
+hotel_room = 100
+tax = hotel_room * 0.08
+total = hotel_room + tax
+room_guests = 4
+share_per_person = total/room_guests 
+ 
+# This line outputs the result of the final calculation stored
+# in the variable "share_per_person"
+print("Each person needs to pay: " + str(share_per_person)) # change a data type
