f4a57f707f2f0e15827fec58f7e92d54040a771e
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-07 19:54:42 +0530

    Update Day 13.py

--- Day 13.py
+++ Day 13.py
@@ -26,3 +26,24 @@ def round_up(number):
  
 # Calls the function with the parameter value of 35.
 print(round_up(35)) # Should print 40
+
+
+
+
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks =filesize // block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder = filesize % block_size
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return(full_blocks + 1) * block_size
+    return full_blocks * block_size
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
