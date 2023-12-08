11d17a84a7eb574746c358ec695fd2d7106a3ce9
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-08 20:02:06 +0530

    Create Day 14.py

---
+++ Day 14.py
@@ -0,0 +1,5 @@
+def getSum(a,b): # Definition of the function
+  mysum = a + b # Adding a and b and assigning the result in variable mysum
+  return mysum # returning mysum value
+
+print(getSum(5,6))
